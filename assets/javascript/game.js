9e6c75c5ab8731edf3a9c14ee5c137171003f969
Bhavesh Patel <kash@Kash.home>
2017-03-05 23:18:31 -0500

    Add crystal logic.

---
+++ assets/javascript/game.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
+	$('#win').text(wins);
+	$('#loss').text(losses);
+	
+	newGame();
+
+	function newGame() {
+
+		counter = 0;
+		
+		// Define random values on new game
+		var redValue = randomIntFromInterval(1, 12);
+		var blueValue = randomIntFromInterval(1, 12);
+		var yellowValue = randomIntFromInterval(1, 12);
+		var greenValue = randomIntFromInterval(1, 12);
+
+		$('#myScore').text(counter);
+
+		function randomIntFromInterval(min, max) {
+			return Math.floor(Math.random()*(max-min+1)+min);
+		}
+
+		var numberToGuess = randomIntFromInterval(19, 120);
+
+		$('.value').text(numberToGuess);
+		
+		// Handle Red Stone Click Event
+		$('#redCrystal').click(function() {
+			counter = counter + redValue;
+			$('#myScore').text(counter);
+
+		    if (counter == numberToGuess) {
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      newGame();
+		        
+		    } else if (counter > numberToGuess) {
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        newGame();
+		    }
+		});
+		
+		// Handle Red Stone Click Event
+		$('#blueCrystal').click(function() {
+			counter = counter + blueValue;
+			$('#myScore').text(counter);
+
+		    if (counter == numberToGuess) {
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      newGame();
+		        
+		    } else if (counter > numberToGuess) {
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        newGame();
+		    }
+		});
+
+		$('#yellowCrystal').click(function() {
+			counter = counter + yellowValue;
+			$('#myScore').text(counter);
+
+		    if (counter == numberToGuess) {
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      newGame();
+		        
+		    } else if (counter > numberToGuess) {
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        newGame();
+		    }
+		});
+		
+		$('#greenCrystal').click(function() {
+			counter = counter + greenValue;
+			$('#myScore').text(counter);
+
+		    if (counter == numberToGuess) {
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      newGame();
+		        
+		    } else if (counter > numberToGuess) {
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        newGame();
+		    }
+		});
+
+	}
+
+});
